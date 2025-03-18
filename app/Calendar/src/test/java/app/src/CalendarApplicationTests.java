3d08d2625b30f817c4be2ff69badbeaf27d8f21a
Xavier Giguère <xavier.giguere@lcieducation.net>
2025-03-18 17:28:02 -0400

    add spring web, redis driver

---
+++ app/Calendar/src/test/java/app/src/CalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package app.src;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
