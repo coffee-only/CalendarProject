0e8db9c8204628dc62cb3fb5ac4cccb6dbe31be0
Lililatortue <will.descoteaux@gmail.com>
2025-05-30 21:53:33 -0400

    basic skeleton class

---
+++ app/src/main/java/api/Users/UserRepository.java
@@ -0,0 +1,39 @@
+package api.Users;
+
+
+
+
+/* DOCUMENTATION:
+ * spring Repositories
+ * https://docs.spring.io/spring-data/data-commons/docs/1.6.1.RELEASE/reference/html/repositories.html
+ * https://www.youtube.com/watch?v=9SGDpanrc8U a 40min
+ *
+ *
+ * */
+
+public class UserRepository{
+
+
+  public void Create(UserDTO user, String psw) {
+    //TODO: not sure how to connect yet
+  }
+
+  public void Update(UserDTO user) {
+    
+  }
+
+  public void Delete(String email, String psw) {
+    //requiert le email et le mot de passe pour delete
+  }
+
+  public List<UserModel>Read() {
+      
+  }
+
+  public UserModel getInfo(int id){
+    
+
+    return;
+  }
+
+}
