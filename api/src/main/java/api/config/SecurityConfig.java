66469c4117378e4f95180ba2616890d5f8fd3701
Lililatortue <will.descoteaux@gmail.com>
2026-02-10 13:12:19 -0500

    added env file configure(not tested) OAuth2

--- api/src/main/java/api/config/SecurityConfig.java
+++ api/src/main/java/api/config/SecurityConfig.java
@@ -14,7 +14,6 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
-        .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/user/register").permitAll()
             .requestMatchers("/actuator/**").permitAll()
