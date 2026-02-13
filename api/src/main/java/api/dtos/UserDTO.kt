502d91e1766cebdd1c848397f3f1a4e868cad1ff
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-02-13 13:51:43 -0500

    Update UserDTO and GroupDto definitions
    - Rename `name` to `username` in UserDTO
    - Add `firstname`, `lastname`, and `creationDate` to UserDTO
    - Add `creationDate` to GroupDto

--- api/src/main/java/api/dtos/UserDTO.kt
+++ api/src/main/java/api/dtos/UserDTO.kt
@@ -1,9 +1,14 @@
 package api.dtos
 
+import java.time.LocalDate
+
 
 data class UserDTO(
     val id: Long,
-    var name: String,
+    var username: String,
     var email: String,
+    val firstname: String? = null,
+    val lastname: String? = null,
+    val creationDate: LocalDate = LocalDate.now(),
     // ommits password for security reasons
 )
