9c2787ec1b86c0b97c7eb08a13d309b583fdeaf0
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-02-13 13:52:49 -0500

    Update User and Group mappers
    - Update `UserMapper` to include `firstname`, `lastname`, and `creationDate` fields in conversions.
    - Correct `username` mapping in `UserMapper` to use `username` property instead of `name`.
    - Add `creationDate` mapping to `GroupMapper` conversions.

--- api/src/main/java/api/maps/GroupMapper.kt
+++ api/src/main/java/api/maps/GroupMapper.kt
@@ -11,6 +11,7 @@ fun GroupEntity.toDto(): GroupDto = GroupDto(
     id = this.id,
     name = this.name,
     ownerId = this.ownerId,
+    creationDate = this.creationDate,
     members = this.members
         .map(UserEntity::toDto)
 )
@@ -19,6 +20,7 @@ fun GroupDto.toEntity(userRepo: UserRepository): GroupEntity = GroupEntity(
     id = this.id,
     name = this.name,
     ownerId = this.ownerId,
+    creationDate = this.creationDate,
     members = this.members.map {
         userRepo.findById(it.id)
             .orElseThrow { UserNotFoundException("User with id ${it.id} Not Found") }
