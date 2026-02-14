7e791dc292aa5f0c1ab682e3805ff44f33dcbf03
Lililatortue <will.descoteaux@gmail.com>
2026-02-14 10:22:10 -0500

    redirect logs to stdout & stderr

--- api/src/main/java/api/Application.kt
+++ api/src/main/java/api/Application.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import java.sql.DriverManager.println
 import javax.sql.DataSource
+import
 
 @SpringBootApplication
 class Application(
@@ -29,6 +30,7 @@ class Application(
         @JvmStatic
         fun main(args: Array<String>) {
             SpringApplication.run(Application::class.java, *args)
+
         }
     }
 }
